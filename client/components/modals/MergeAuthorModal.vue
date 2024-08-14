1711e2e85fafb1768c0a750e617948eb14155acf
rk-763 <kihomura552@gmail.com>
2024-08-14 14:32:05 +1200

    Modified MergeAuthorModal.vue, make sure it can implement different merge functions.

--- client/components/modals/MergeAuthorModal.vue
+++ client/components/modals/MergeAuthorModal.vue
@@ -1,72 +1,118 @@
 <template>
-  <div class="modal-overlay">
+  <div class="modal-overlay" @click.self="close">
     <div class="modal-container">
-      <h2 class="modal-title">Merge Authors</h2>
-      <div class="author-sections">
-        <div class="author-section">
-          <h3>Author A</h3>
-          <div class="author-details">
-            <label>
-              <input type="checkbox" v-model="selectedAuthorAName" @change="updateMergedAuthorName('A')" />
-              Name: {{ authorA.name }}
-            </label>
-            <label>
-              <input type="checkbox" v-model="selectedAuthorAImage" @change="updateMergedAuthorImage('A')" />
-              <covers-author-image :author="authorA" />
-            </label>
-            <label>
-              <input type="checkbox" v-model="selectedAuthorAASIN" @change="updateMergedAuthorASIN('A')" />
-              ASIN: {{ authorA.asin }}
-            </label>
-            <label>
-              <input type="checkbox" v-model="selectedAuthorADescription" @change="updateMergedAuthorDescription('A')" />
-              Description: {{ authorA.description }}
-            </label>
-            <label v-for="(alias, index) in authorA.alias" :key="index">
-              <input type="checkbox" v-model="selectedAuthorAAlias[index]" @change="updateMergedAuthorAlias('A', index)" />
-              Alias: {{ alias }}
-            </label>
-          </div>
-        </div>
-        <div class="author-section">
-          <h3>Author B</h3>
-          <div class="author-details">
-            <label>
-              <input type="checkbox" v-model="selectedAuthorBName" @change="updateMergedAuthorName('B')" />
-              Name: {{ authorB.name }}
-            </label>
-            <label>
-              <input type="checkbox" v-model="selectedAuthorBImage" @change="updateMergedAuthorImage('B')" />
-              <covers-author-image :author="authorB" />
-            </label>
-            <label>
-              <input type="checkbox" v-model="selectedAuthorBASIN" @change="updateMergedAuthorASIN('B')" />
-              ASIN: {{ authorB.asin }}
-            </label>
-            <label>
-              <input type="checkbox" v-model="selectedAuthorBDescription" @change="updateMergedAuthorDescription('B')" />
-              Description: {{ authorB.description }}
-            </label>
-            <label v-for="(alias, index) in authorB.alias" :key="index">
-              <input type="checkbox" v-model="selectedAuthorBAlias[index]" @change="updateMergedAuthorAlias('B', index)" />
-              Alias: {{ alias }}
-            </label>
-          </div>
-        </div>
-        <div class="merged-author-section">
-          <h3>Merged Author</h3>
-          <div class="author-details">
-            <p>Name: {{ mergedAuthor.name }}</p>
-            <covers-author-image :author="mergedAuthor" />
-            <p>ASIN: {{ mergedAuthor.asin }}</p>
-            <p>Description: {{ mergedAuthor.description }}</p>
-            <p>Alias: {{ mergedAuthor.alias }}</p>
-          </div>
-        </div>
+
+      <div class="sidebar">
+        <button @click="setActiveTab('merge')" :class="{ active: activeTab === 'merge' }">Merge</button>
+        <button @click="setActiveTab('makeAlias')" :class="{ active: activeTab === 'makeAlias' }">Make Alias</button>
       </div>
-      <div class="modal-actions">
-        <button class="btn btn-primary" @click="mergeAuthors">Merge</button>
-        <button class="btn btn-secondary" @click="close">Cancel</button>
+
+      <div class="main-content">
+        <h2 class="modal-title" v-if="activeTab === 'merge'">Merge Authors</h2>
+        <h2 class="modal-title" v-else>Make Alias</h2>
+
+        <!-- merge modal -->
+        <div v-if="activeTab === 'merge'" class="author-sections">
+          <div class="author-sections">
+            <!-- Author A Section -->
+            <div class="author-section">
+              <h3>{{ authorA.name }}</h3>
+              <div class="author-details">
+                <label>
+                  <input type="radio" name="name" v-model="selectedAuthor" value="A" @change="updateMergedAuthorName('A')" />
+                  Name: {{ authorA.name }}
+                </label>
+                <label>
+                  <input type="radio" name="image" v-model="selectedImage" value="A" @change="updateMergedAuthorImage('A')" />
+                  <div class="author-image-container">
+                    <covers-author-image :author="authorA" :default-image="defaultImage" />
+                  </div>
+                </label>
+                <label>
+                  <input type="radio" name="asin" v-model="selectedASIN" value="A" @change="updateMergedAuthorASIN('A')" />
+                  ASIN: {{ authorA.asin }}
+                </label>
+                <label>
+                  <input type="radio" name="description" v-model="selectedDescription" value="A" @change="updateMergedAuthorDescription('A')" />
+                  Description: {{ authorA.description }}
+                </label>
+                <label v-for="(alias, index) in authorA.alias" :key="index">
+                  <input type="radio" :name="'alias' + index" v-model="selectedAlias[index]" :value="`A${index}`" @change="updateMergedAuthorAlias('A', index)" />
+                  Alias: {{ alias }}
+                </label>
+              </div>
+            </div>
+
+            <!-- Author B Section -->
+            <div class="author-section">
+              <h3>{{ authorB.name }}</h3>
+              <div class="author-details">
+                <label>
+                  <input type="radio" name="name" v-model="selectedAuthor" value="B" @change="updateMergedAuthorName('B')" />
+                  Name: {{ authorB.name }}
+                </label>
+                <label>
+                  <input type="radio" name="image" v-model="selectedImage" value="B" @change="updateMergedAuthorImage('B')" />
+                  <div class="author-image-container">
+                    <covers-author-image :author="authorB" :default-image="defaultImage" />
+                  </div>
+                </label>
+                <label>
+                  <input type="radio" name="asin" v-model="selectedASIN" value="B" @change="updateMergedAuthorASIN('B')" />
+                  ASIN: {{ authorB.asin }}
+                </label>
+                <label>
+                  <input type="radio" name="description" v-model="selectedDescription" value="B" @change="updateMergedAuthorDescription('B')" />
+                  Description: {{ authorB.description }}
+                </label>
+                <label v-for="(alias, index) in authorB.alias" :key="index">
+                  <input type="radio" :name="'alias' + index" v-model="selectedAlias[index]" :value="`B${index}`" @change="updateMergedAuthorAlias('B', index)" />
+                  Alias: {{ alias }}
+                </label>
+              </div>
+            </div>
+
+            <!-- Merged Author Section -->
+            <div class="merged-author-section">
+              <h3>Merged Author</h3>
+              <div class="author-details">
+                <p>Name: {{ mergedAuthor.name }}</p>
+                <div class="author-image-container">
+                  <covers-author-image :author="mergedAuthor" :default-image="defaultImage" />
+                </div>
+                <p>ASIN: {{ mergedAuthor.asin }}</p>
+                <p>Description: {{ mergedAuthor.description }}</p>
+                <p>Alias: {{ mergedAuthor.alias.join(', ') }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- make alias modal -->
+        <div v-if="activeTab === 'makeAlias'" class="author-sections">
+          <div class="author-section">
+            <h3>{{ authorA.name }}</h3>
+            <div class="author-image-container">
+              <covers-author-image :author="authorA" :default-image="defaultImage" />
+            </div>
+            <button class="btn btn-make-alias mt-2" @click="mergeAliasesToB">Make A to B's Alias</button>
+          </div>
+
+          <div class="author-section">
+            <h3>{{ authorB.name }}</h3>
+            <div class="author-image-container">
+              <covers-author-image :author="authorB" :default-image="defaultImage" />
+            </div>
+            <button class="btn btn-make-alias mt-2" @click="mergeAliasesToA">Make B to A's Alias</button>
+          </div>
+      </div>
+
+        <!--  cancel button-->
+        <div class="modal-actions">
+          <button v-if="activeTab === 'merge'" class="btn btn-primary" @click="mergeAuthors">Merge</button>
+          <button class="btn btn-secondary" @click="close">Cancel</button>
+        </div>
+
       </div>
     </div>
   </div>
@@ -91,6 +137,7 @@ export default {
   },
   data() {
     return {
+      activeTab: 'merge',
       mergedAuthor: {
         name: '',
         imagePath: '',
@@ -98,86 +145,73 @@ export default {
         description: '',
         alias: []
       },
-      selectedAuthorAName: false,
-      selectedAuthorAImage: false,
-      selectedAuthorAASIN: false,
-      selectedAuthorADescription: false,
-      selectedAuthorAAlias: [],
-      selectedAuthorBName: false,
-      selectedAuthorBImage: false,
-      selectedAuthorBASIN: false,
-      selectedAuthorBDescription: false,
-      selectedAuthorBAlias: [],
-      searching: false
+      selectedAuthor: 'A',
+      selectedImage: 'A',
+      selectedASIN: 'A',
+      selectedDescription: 'A',
+      selectedAlias: {},
+      defaultImage: '/path/to/default-image.png' // 提供图像占位符路径
     }
   },
   methods: {
-    updateMergedAuthorName(author) {
-      if (author === 'A') {
-        this.mergedAuthor.name = this.selectedAuthorAName ? this.authorA.name : this.authorB.name
-      } else {
-        this.mergedAuthor.name = this.selectedAuthorBName ? this.authorB.name : this.authorA.name
+    setActiveTab(tab) {
+      this.activeTab = tab;
+    },
+    async makeAlias(direction) {
+      try {
+        const payload = {
+          authorAId: this.authorA.id,
+          authorBId: this.authorB.id,
+          direction // 'AtoB' 或 'BtoA'
+        }
+        const response = await this.$axios.post('/api/authors/makeAlias', payload)
+        this.$toast.success(`Alias created successfully: ${direction}`)
+        this.$emit('make-alias', response.data)
+        this.close()
+      } catch (error) {
+        this.$toast.error('Failed to make alias')
+        console.error('Make alias error:', error)
       }
+    },
+    updateMergedAuthorName(author) {
+      this.mergedAuthor.name = author === 'A' ? this.authorA.name : this.authorB.name
     },
     updateMergedAuthorImage(author) {
-      if (author === 'A') {
-        this.mergedAuthor.imagePath = this.selectedAuthorAImage ? this.authorA.imagePath : this.authorB.imagePath
-      } else {
-        this.mergedAuthor.imagePath = this.selectedAuthorBImage ? this.authorB.imagePath : this.authorA.imagePath
-      }
+      this.mergedAuthor.imagePath = author === 'A' ? this.authorA.imagePath : this.authorB.imagePath
     },
     updateMergedAuthorASIN(author) {
-      if (author === 'A') {
-        this.mergedAuthor.asin = this.selectedAuthorAASIN ? this.authorA.asin : this.authorB.asin
-      } else {
-        this.mergedAuthor.asin = this.selectedAuthorBASIN ? this.authorB.asin : this.authorA.asin
-      }
+      this.mergedAuthor.asin = author === 'A' ? this.authorA.asin : this.authorB.asin
     },
     updateMergedAuthorDescription(author) {
-      if (author === 'A') {
-        this.mergedAuthor.description = this.selectedAuthorADescription ? this.authorA.description : this.authorB.description
-      } else {
-        this.mergedAuthor.description = this.selectedAuthorBDescription ? this.authorB.description : this.authorA.description
-      }
+      this.mergedAuthor.description = author === 'A' ? this.authorA.description : this.authorB.description
     },
     updateMergedAuthorAlias(author, index) {
-      if (author === 'A') {
-        this.$set(this.mergedAuthor.alias, index, this.selectedAuthorAAlias[index] ? this.authorA.alias[index] : this.authorB.alias[index])
-      } else {
-        this.$set(this.mergedAuthor.alias, index, this.selectedAuthorBAlias[index] ? this.authorB.alias[index] : this.authorA.alias[index])
-      }
+      this.$set(this.mergedAuthor.alias, index, author === 'A' ? this.authorA.alias[index] : this.authorB.alias[index])
     },
-    mergeAuthors() {
-      this.$emit('merge', this.mergedAuthor)
-      this.close()
+    mergeAliasesToB() {
+      this.mergedAuthor.alias = [...new Set([...this.authorB.alias, ...this.authorA.alias])]
+    },
+    mergeAliasesToA() {
+      this.mergedAuthor.alias = [...new Set([...this.authorA.alias, ...this.authorB.alias])]
+    },
+    async mergeAuthors() {
+      try {
+        const payload = {
+          authorAId: this.authorA.id,
+          authorBId: this.authorB.id,
+          mergedAuthorData: this.mergedAuthor
+        }
+        const response = await this.$axios.post('/api/authors/merge', payload)
+        this.$toast.success('Authors merged successfully')
+        this.$emit('merge', response.data)
+        this.close()
+      } catch (error) {
+        this.$toast.error('Failed to merge authors')
+        console.error('Merge error:', error)
+      }
     },
     close() {
       this.$emit('close')
-    },
-    async searchAuthor(author) {
-      this.searching = true
-      const payload = {}
-      if (author.asin) payload.asin = author.asin
-      else payload.q = author.name
-
-      payload.region = 'us'
-      if (this.libraryProvider.startsWith('audible.')) {
-        payload.region = this.libraryProvider.split('.').pop() || 'us'
-      }
-
-      const response = await this.$axios.$post(`/api/authors/${author.id}/match`, payload).catch((error) => {
-        console.error('Failed', error)
-        return null
-      })
-      if (!response) {
-        this.$toast.error(`Author ${author.name} not found`)
-      } else if (response.updated) {
-        if (response.author.imagePath) this.$toast.success(`Author ${response.author.name} was updated`)
-        else this.$toast.success(`Author ${response.author.name} was updated (no image found)`)
-      } else {
-        this.$toast.info(`No updates were made for Author ${response.author.name}`)
-      }
-      this.searching = false
     }
   }
 }
@@ -200,9 +234,12 @@ export default {
   background-color: #1a1a1a;
   padding: 20px;
   border-radius: 10px;
-  width: 800px;
+  width: 70%;
+  max-width: 600px;
   max-height: 80vh;
-  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
 }
 
 .modal-title {
@@ -214,12 +251,15 @@ export default {
 .author-sections {
   display: flex;
   justify-content: space-between;
+  flex-grow: 1;
+  overflow-y: auto;
 }
 
 .author-section,
 .merged-author-section {
   flex: 1;
-  margin: 0 10px;
+  margin-right: 10px;
+  max-width: 30%;
 }
 
 .author-details {
@@ -231,14 +271,25 @@ export default {
   margin-bottom: 10px;
 }
 
+.author-image {
+  max-width: 100px;
+  max-height: 100px;
+  object-fit: cover;
+  border-radius: 10px;
+  margin-bottom: 10px;
+}
+
 .merged-author-section {
-  border-left: 2px solid #333;
-  padding-left: 20px;
+  border-left: none;
+  padding-left: 0;
 }
 
 .modal-actions {
   text-align: center;
-  margin-top: 20px;
+  margin-top: auto;
+  padding-top: 10px;
+  border-top: 1px solid #333;
+  position: relative;
 }
 
 .btn {
@@ -265,5 +316,110 @@ export default {
 
 .btn-secondary:hover {
   background-color: #4b5563;
+}
+
+.author-image-container {
+  background-image: url('/path/to/default-image.png');
+  background-size: cover;
+  background-position: center;
+  width: 100px;
+  height: 100px;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-container {
+  background-color: #1a1a1a;
+  padding: 20px;
+  border-radius: 10px;
+  width: 70%;
+  max-width: 800px;
+  max-height: 80vh;
+  display: flex;
+  flex-direction: row;
+  overflow: hidden;
+}
+
+.sidebar {
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+  margin-right: 20px;
+}
+
+.sidebar button {
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  background-color: #333;
+  color: white;
+  cursor: pointer;
+}
+
+.sidebar button.active {
+  background-color: #2563eb;
+}
+
+.main-content {
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+}
+
+.modal-title {
+  text-align: center;
+  font-size: 24px;
+  margin-bottom: 20px;
+}
+
+.author-section {
+  margin-bottom: 20px;
+}
+
+.btn {
+  padding: 0.5rem 1rem;
+  border-radius: 0.375rem;
+  font-weight: 500;
+  cursor: pointer;
+}
+
+.btn-primary {
+  background-color: #2563eb;
+  color: #ffffff;
+}
+
+.btn-primary:hover {
+  background-color: #1d4ed8;
+}
+
+.btn-secondary {
+  background-color: #6b7280;
+  color: #ffffff;
+  margin-left: 10px;
+}
+
+.btn-secondary:hover {
+  background-color: #4b5563;
+}
+
+.btn-make-alias {
+  background-color: #34d399;
+  color: #1a1a1a;
+}
+
+.btn-make-alias:hover {
+  background-color: #10b981;
 }
 </style>
